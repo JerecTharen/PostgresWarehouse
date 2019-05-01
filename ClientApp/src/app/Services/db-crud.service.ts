74a1555f6e3b3739ec1ce579e557285060a36428
JerecTharen <kf7ags@gmail.com>
2019-05-01 10:56:12 -0600

    Building crud service

--- ClientApp/src/app/Services/db-crud.service.ts
+++ ClientApp/src/app/Services/db-crud.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Component, OnInit, Inject, Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbCRUDService {
 
-  constructor() { }
+  constructor(
+    http: HttpClient,
+    @Inject('BASE_URL') baseUrl: string
+  ) {
+    
+   }
 }
